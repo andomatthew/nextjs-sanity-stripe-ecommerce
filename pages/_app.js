c8fb47f8002f95991e5d3a58f6dcc5f59fd9030a
Alexandro M <alexandro.matthew@gmail.com>
2023-01-11 09:49:01 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { Layout } from '../components'
+
+import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
+import { StateContext } from '../context/stateContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
 
 export default MyApp
